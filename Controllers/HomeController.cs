23bae94903dd6fe57d621c6c28a8e093644c1db4
KatherinneAlgarin <katherinne.cruz1@catolica.edu.sv>
2026-07-10 19:18:17 -0600

    Fisrt commit - diseño pagina inicial con boostrap

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using KatherinneCruz.Lab01.Models;
+
+namespace KatherinneCruz.Lab01.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.Nombre = "Katherinne Jeantte Cruz Algarin";
+        ViewBag.Carrera = "Ingeniería en Desarrollo de Software";
+        ViewBag.Asignatura = "Desarrollo Multiplataforma de Aplicaciones Web";
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KatherinneCruz.Lab01.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
